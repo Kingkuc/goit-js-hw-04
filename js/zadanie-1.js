aa4d37eb861a1b74e8773eb6e5306ab8db53fe8c
KingKuc <129443521+Kingkuc@users.noreply.github.com>
2024-05-20 22:34:30 +0200

    1 completed

--- js/zadanie-1.js
+++ js/zadanie-1.js
@@ -1,18 +1,17 @@
 // Zadanie 1. Pakowanie towarów
 function isEnoughCapacity(products, containerSize) {
-    const products {...arguments};
         const values = Object.values(products);
         let totalValue = 0;
 
-        for ( const value of values) {
+        for (const value of values) {
             totalValue += value;
-        };
+        }
+        
         if (totalValue >= containerSize) {
-            return true;}
-            else {return false;
+            return true;
+        }   else return false;
+        
+}
 
-            }
-        };
-    
 
     
