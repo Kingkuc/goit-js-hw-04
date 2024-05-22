33d3fabac21b50aaffdfdd911f135a4f635a93dc
KingKuc <129443521+Kingkuc@users.noreply.github.com>
2024-05-22 06:57:49 +0200

    fixed

--- js/zadanie-3.js
+++ js/zadanie-3.js
@@ -9,6 +9,15 @@ const profile = {
     this.playTime += hours;
     },
     getInfo(){
-        console.log(`${Username} ma ${amount} aktywnych godzin!`);
+        console.log(`${newName} ma ${this.playTime} aktywnych godzin!`);
     }
 };
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
